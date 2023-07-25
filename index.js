e2b193c7e1511287ff228a3ab5085d1acd3c218f
Maciej Sikora <maciej.sikora.1999@wp.pl>
2023-07-25 22:05:10 +0200

    Uodate PersistGate

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter basename="goit-react-hw-08-phonebook">
-        <PersistGate loading={<div className="loading">Loading...</div>}>
+        <PersistGate loading={null} persistor={persistor}>
           <App />
         </PersistGate>
       </BrowserRouter>
